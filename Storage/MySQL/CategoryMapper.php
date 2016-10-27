0d9eeb12e60b0f793b999948cbb3e7a0d2a776b0
David Yang <daworld.ny@gmail.com>
2016-10-28 01:03:45 +0500

    Implementation of category mapper for MySQL

---
+++ Storage/MySQL/CategoryMapper.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Quiz\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractMapper;
+use Quiz\Storage\CategoryMapperInterface;
+use Krystal\Db\Sql\RawSqlFragment;
+
+final class CategoryMapper extends AbstractMapper implements CategoryMapperInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public static function getTableName()
+    {
+        return self::getWithPrefix('bono_module_quiz_categories');
+    }
+
+    /**
+     * Fetch all categories
+     * 
+     * @param boolean $sort Whether to use sorting by order attribute or not
+     * @return array
+     */
+    public function fetchAll($sort)
+    {
+        $db = $this->db->select('*')
+                       ->from(self::getTableName())
+                       ->whereEquals('lang_id', $this->getLangId());
+
+        if ($sort === true) {
+            $db->orderBy(new RawSqlFragment('`order`, CASE WHEN `order` = 0 THEN `id` END DESC'));
+        } else {
+            $db->orderBy('id')
+               ->desc();
+        }
+
+        return $db->queryAll();
+    }
+
+    /**
+     * Deletes a category by its associated id
+     * 
+     * @param string $id
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        return $this->deleteByPk($id);
+    }
+
+    /**
+     * Finds a category by its associated id
+     * 
+     * @param string $id
+     * @return array
+     */
+    public function fetchById($id)
+    {
+        return $this->findByPk($id);
+    }
+
+    /**
+     * Inserts a category
+     * 
+     * @param array $data
+     * @return boolean
+     */
+    public function insert(array $data)
+    {
+        return $this->persist($this->getWithLang($data));
+    }
+
+    /**
+     * Updates a category
+     * 
+     * @param array $data
+     * @return boolean
+     */
+    public function update(array $data)
+    {
+        return $this->persist($data);
+    }
+}
