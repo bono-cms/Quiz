cbce65d302d996abbf1113df7e745eb74f71932b
David Yang <daworld.ny@gmail.com>
2016-10-27 23:30:58 +0500

    Fixed route path

--- Config/module.config.php
+++ Config/module.config.php
@@ -3,7 +3,7 @@
 return array(
  'name'  => 'Quiz',
  'caption'  => 'Quiz',
- 'route' => 'Quiz:Admin:Faq@gridAction',
+ 'route' => 'Quiz:Admin:Browser@indexAction',
  'icon'  => 'fa fa-book fa-5x',
  'order' => 1,
  'description' => 'Lets you manage online quizes on your site',
