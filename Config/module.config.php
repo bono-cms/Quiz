7db50b0f659306efc88d2b6f7cef7f73f9acfffb
David Yang <daworld.ny@gmail.com>
2016-10-27 22:02:38 +0500

    Initial module configuration

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,5 +1,10 @@
 <?php
 
 return array(
-    
+ 'name'  => 'Quiz',
+ 'caption'  => 'Quiz',
+ 'route' => 'Quiz:Admin:Faq@gridAction',
+ 'icon'  => 'fa fa-book fa-5x',
+ 'order' => 1,
+ 'description' => 'Lets you manage online quizes on your site',
 );
