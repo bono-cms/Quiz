4d942b4fe2e200a21ba391b4152be84eb1326018
David Yang <daworld.ny@gmail.com>
2016-10-28 23:48:10 +0500

    Basic grid implementation

--- Controller/Admin/Browser.php
+++ Controller/Admin/Browser.php
@@ -15,7 +15,51 @@ use Cms\Controller\Admin\AbstractController;
 
 final class Browser extends AbstractController
 {
+    /**
+     * Creates the grid
+     * 
+     * @param string $id Category id
+     * @param string $page Page number
+     * @return string
+     */
+    private function createGrid($id, $page)
+    {
+        // Append a breadcrumb
+        $this->view->getBreadcrumbBag()
+                   ->addOne('Quiz');
+
+        $paginator = $this->getModuleService('questionService')->getPaginator();
+        $paginator->setUrl($this->createUrl('Quiz:Admin:Browser@categoryAction', array($id, $page), 0));
+
+        return $this->view->render('browser', array(
+            'categoryId' => $id,
+            'questions' => $this->getModuleService('questionService')->fetchAllByCategoryId($id, $page, $this->getSharedPerPageCount()),
+            'categories' => $this->getModuleService('categoryService')->fetchAll(false),
+            'paginator' => $paginator
+        ));
+    }
+
+    /**
+     * Renders the grid filtering by category id
+     * 
+     * @param string $id Category id
+     * @param string $page Page number
+     * @return string
+     */
+    public function categoryAction($id, $page = 1)
+    {
+        return $this->createGrid($id, $page);
+    }
+
+    /**
+     * Renders the grid
+     * 
+     * @param string $page Page number
+     * @return string
+     */
     public function indexAction()
     {
+        $id = $this->getModuleService('categoryService')->getLastId();
+        return $this->createGrid($id, 1);
     }
 }
