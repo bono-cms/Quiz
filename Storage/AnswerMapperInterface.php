a195bd203ca0bc34eace79334e56f260699412a7
David Yang <daworld.ny@gmail.com>
2016-11-02 17:45:59 +0500

    Added answer mapper

---
+++ Storage/AnswerMapperInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Quiz\Storage;
+
+interface AnswerMapperInterface
+{
+    /**
+     * Fetch all answers
+     * 
+     * @param string $id Question id
+     * @param boolean $sort Whether to use sorting by order attribute or not
+     * @return array
+     */
+    public function fetchAll($id, $sort);
+
+    /**
+     * Fetches an answer by its associated id
+     * 
+     * @param string $id Answer id
+     * @return array
+     */
+    public function fetchById($id);
+
+    /**
+     * Inserts an answer
+     * 
+     * @param array $answer
+     * @return boolean
+     */
+    public function insert(array $answer);
+
+    /**
+     * Updates an answer
+     * 
+     * @param array $answer
+     * @return boolean
+     */
+    public function update(array $answer);
+
+    /**
+     * Deletes an answer by its associated id
+     * 
+     * @param string $id Answer id
+     * @return boolean
+     */
+    public function deleteById($id);
+}
