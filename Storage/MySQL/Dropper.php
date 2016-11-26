a4ada78f6e7ce5c01ff51f2f8341f6ba858b9a8d
David Yang <daworld.ny@gmail.com>
2016-11-26 21:49:47 +0500

    Added missing module dropper

---
+++ Storage/MySQL/Dropper.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Quiz\Storage\MySQL;
+
+use Cms\Storage\MySQL\AbstractStorageDropper;
+
+final class Dropper extends AbstractStorageDropper
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function getTables()
+    {
+        return array(
+            AnswerMapper::getTableName(),
+            CategoryMapper::getTableName(),
+            HistoryMapper::getTableName(),
+            QuestionMapper::getTableName()
+        );
+    }
+}
