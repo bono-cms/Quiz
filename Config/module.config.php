2076afaaad8b6e2e635d909d560f2db0d833f03f
dave-yang <daworld.ny@gmail.com>
2018-12-17 19:57:17 +0500

    Minor fixes. Use shared action partial

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
  'name'  => 'Quiz',
  'caption'  => 'Quiz',
  'route' => 'Quiz:Admin:Browser@indexAction',
- 'icon'  => 'fa fa-book fa-5x',
  'order' => 1,
  'description' => 'Lets you manage online quizes on your site',
 );
