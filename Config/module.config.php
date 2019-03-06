41c6b3a29954810c3ad9e6e5b4a4c56febd75517
dave-yang <daworld.ny@gmail.com>
2019-03-06 23:07:10 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,36 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'name'  => 'Quiz',
- 'caption'  => 'Quiz',
- 'route' => 'Quiz:Admin:Browser@indexAction',
- 'order' => 1,
- 'description' => 'Lets you manage online quizes on your site',
+    'name'  => 'Quiz',
+    'description' => 'Lets you manage online quizes on your site',
+    'menu' => array(
+        'name' => 'Quiz',
+        'icon' => 'fas fa-chart-area',
+        'items' => array(
+            array(
+                'route' => 'Quiz:Admin:Browser@indexAction',
+                'name' => 'View all'
+            ),
+            array(
+                'route' => 'Quiz:Admin:Question@addAction',
+                'name' => 'Add new question'
+            ),
+            array(
+                'route' => 'Quiz:Admin:Config@indexAction',
+                'name' => 'Configuration'
+            ),
+            array(
+                'route' => 'Quiz:Admin:Category@addAction',
+                'name' => 'Add new category'
+            ),
+            array(
+                'route' => 'Quiz:Admin:History@indexAction',
+                'name' => 'History'
+            )
+        )
+    )
 );
