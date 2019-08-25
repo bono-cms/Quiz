44406b904065a2bd77b50c152912b019a760b260
dave-yang <daworld.ny@gmail.com>
2019-08-25 18:39:47 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,14 +1,16 @@
 
+/* Categories */
 DROP TABLE IF EXISTS `bono_module_quiz_categories`;
 CREATE TABLE `bono_module_quiz_categories` (
-  
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `lang_id` INT NOT NULL,
   `name` varchar(255) NOT NULL COMMENT 'Category name',
-  `order` INT NOT NULL COMMENT 'Category sorting order'
-  
-) DEFAULT CHARSET = UTF8;
+  `order` INT NOT NULL COMMENT 'Category sorting order',
 
+  FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
+) DEFAULT CHARSET=UTF8 ENGINE = InnoDB;
+
+/* Questions */
 DROP TABLE IF EXISTS `bono_module_quiz_questions`;
 CREATE TABLE `bono_module_quiz_questions` (
   
@@ -16,28 +18,30 @@ CREATE TABLE `bono_module_quiz_questions` (
   `category_id` INT NOT NULL,
   `question` varchar(255) NOT NULL COMMENT 'Question',
   `order` INT NOT NULL COMMENT 'Sorting order (for non-random mode)',
-  `description` LONGTEXT NOT NULL COMMENT 'Optional description for the question'
-  
-) DEFAULT CHARSET = UTF8;
+  `description` LONGTEXT NOT NULL COMMENT 'Optional description for the question',
 
+  FOREIGN KEY (category_id) REFERENCES bono_module_quiz_categories(id) ON DELETE CASCADE
+
+) DEFAULT CHARSET=UTF8 ENGINE = InnoDB;
+
+/* Answers */
 DROP TABLE IF EXISTS `bono_module_quiz_answers`;
 CREATE TABLE `bono_module_quiz_answers` (
-  
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `question_id` INT NOT NULL,
   `answer` TEXT NOT NULL,
   `order` INT NOT NULL COMMENT 'Sorting order',
-  `correct` varchar(1) NOT NULL COMMENT 'Whether the answer is corrent. Possible values are 0 and 1'
-  
-) DEFAULT CHARSET = UTF8;
+  `correct` BOOLEAN NOT NULL COMMENT 'Whether the answer is corrent. Possible values are 0 and 1',
 
+  FOREIGN KEY (question_id) REFERENCES bono_module_quiz_questions(id) ON DELETE CASCADE  
+) DEFAULT CHARSET=UTF8 ENGINE = InnoDB;
+
+/* History */
 DROP TABLE IF EXISTS `bono_module_quiz_history`;
 CREATE TABLE `bono_module_quiz_history` (
-  
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `category` varchar(30) NOT NULL,
   `name` varchar(30),
   `points` INT NOT NULL,
   `timestamp` INT(10) NOT NULL 
-  
-) DEFAULT CHARSET = UTF8;
+) DEFAULT CHARSET=UTF8 ENGINE = InnoDB;
